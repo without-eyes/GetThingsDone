ca93d706255e2aba84a75dc1d5725e00f3d14b8c
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-05-10 22:09:40 +0300

    Initial commit

---
+++ src/main/java/com/GetThingsDone/GetThingsDoneApplication.java
@@ -0,0 +1,13 @@
+package com.GetThingsDone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetThingsDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetThingsDoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GetThingsDone/GetThingsDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GetThingsDone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetThingsDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
